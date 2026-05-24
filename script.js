2ae6a61fdd269b9a338e9c854bcea3fbbd89fcc6
Sadiya <160082120+sadiyameem@users.noreply.github.com>
2026-05-24 11:40:54 -0400

    made the UI match the expenses from the local storage

--- script.js
+++ script.js
@@ -10,3 +10,32 @@ const expensedate = document.getElementById('expense-date');
 let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
 let eidtingExpenseId = null;
 let deletingExpenseId = null;
+
+
+// Make the UI match the expenses from the local storage
+function updateUI() {
+    expenseTable.innerHTML = '';
+    let total = 0;
+
+    if (expenses.length === 0) {
+        const emptyMessageRow = document.createElement('tr');
+        emptyMessageRow.innerHTML = `<td colspan='5' class='empty-table-message'>No expenses recorded.</td>`;
+        expenseTable.appendChild(emptyMessageRow);
+    } else {
+        expenses.forEach(expense => {
+            const row = document.createElement('tr');
+            row.innerHTML = `
+            <td>${expense.name}</td>
+            <td>$${expenses.amount.toFixed(2)}</td>
+            <td>${expense.category}</td>
+            <td>${expenses.date}
+            </td>
+            <button class='edit-btn' onclick='openEditModal(${expense.id})'>Edit</button>
+            <button class='delete-btn' onclick='openDeleteModal(${expense.id})'>Delete</button>
+            </td>;`
+            expenseTable.appendChild(row);
+            total += expense.amount;
+        });
+    }
+    totalExpenseDisplay.textCount = total.toFixed(2);
+}
